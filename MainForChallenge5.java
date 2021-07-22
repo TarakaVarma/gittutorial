bf8a18d548b4842e8dc55cd340fdb55920b1384f
TarakaVarma <tarakvarma1998@gmail.com>
2021-07-22 07:33:39 +0530

    challenge5 done

---
+++ MainForChallenge5.java
@@ -0,0 +1,12 @@
+package com.TechnoElevateChallenge;
+
+public class MainForChallenge5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		challenge5 c=new challenge5();
+		c.printNumberInWord(-5);
+
+	}
+
+}
---
+++ challenge5.java
@@ -0,0 +1,61 @@
+package com.TechnoElevateChallenge;
+
+import java.util.Scanner;
+
+public class challenge5 {
+
+
+
+
+	 public void printNumberInWord(int wholeNumber) {
+	
+		 
+		 switch(wholeNumber) {
+		     case 0:
+			       System.out.println("zero");
+			 break;
+			 case 1: 
+			 case -1:		 
+				   System.out.println("one");
+			 break;
+			 case 2 :
+			 case -2:	 
+				    System.out.println("two");
+			 break;
+			 case 3 :
+			 case -3:	 
+				    System.out.println("Three");
+			 break;
+			 case 4 :
+			 case -4:	 
+				    System.out.println("Four");
+			 break;
+			 case 5 :
+			 case -5:	 
+				    System.out.println("five");
+			 break;
+			 case 6:
+			 case -6:	 
+				    System.out.println("six");
+			 break;
+			 case 7 :
+			 case -7:	 
+				    System.out.println("seven");
+			 break;
+			 case 8 :
+			 case -8:	 
+				    System.out.println("eight");
+			 break;
+			 case 9 :
+			 case -9:	 
+				    System.out.println("nine");
+			 break;
+			 default:
+				 System.out.println("invalid number..");
+		 }
+	 }
+	 
+
+	}
+
+
